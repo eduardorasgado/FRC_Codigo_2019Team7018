3fab2bbeb92d8930383934192c2184b4b4989aaf
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-03 22:26:53 -0600

    transforming to gray, getting roi, applying thresholding and eliminating false positives

--- visionSystem/squareDetectionAlgo.py
+++ visionSystem/squareDetectionAlgo.py
@@ -15,7 +15,42 @@ def resizingImage(img, f_height, f_width):
         JUST_ONCE = 1
     
     img = cv2.resize(img, (f_height, f_width))
-    return img
+    return img, f_height, f_width
+
+def gettingROI(img, width, height):
+
+    print("[WIDTH]: {} [HEIGHT]: {}".format(width, height))
+    # poligono o rectangulo de abajo de el poligono superior
+    #vertices = np.array([[0, height//2],[width,height//2],[width,height],[0,height]], np.int32)
+
+    # poligono que permanecera al final con la imagen del procesamiento inicial
+    vertices = np.array([[0, 0],[width, 0],[width, height*0.75],[0, height*0.75]], np.int32)
+    vertices = [vertices]
+    # creando un filtro de puros ceros
+    mask = np.zeros_like(img)
+    # trazar el poligino en la mascara de zeros y llenar lo que no nos interesa
+    # con negro
+    cv2.fillPoly(mask, vertices, 255)
+    #fusionar con una compuerta logica and
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
+def processingImage(frame, width, height):
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+
+    # consiguiendo las areas de interes
+    masked = gettingROI(gray, width, height)
+
+    # aplicando un filtro threshold que produce una imagen binaria con los rangos propuestos
+    #img, filtro bajo, filtro alto, algoritmo de filtro
+    ret, threshImg = cv2.threshold(masked, 180, 255, cv2.THRESH_BINARY)
+
+    # eliminando falsos positivos
+    kernel = np.ones((4,4), np.uint8)
+    threshImg = cv2.morphologyEx(threshImg,  cv2.MORPH_OPEN, kernel)
+
+    
+    return gray, masked, threshImg
 
 def mainLoop():
     cap = cv2.VideoCapture("GoPro.mp4")
@@ -35,11 +70,16 @@ def mainLoop():
 
         # procesando la imagen
         #cambiando tamano de la imagen
-        frame = resizingImage(frame, frame_width, frame_height)
-        
+        frame, f_height, f_width = resizingImage(frame, frame_width, frame_height)
+
+        grayScale, masked, threshImg = processingImage(frame, f_height, f_width)
+
         # comprobando que exista un retorno de pantalla
         if ret == True:
              cv2.imshow("Cuadro inicial", frame)
+             cv2.imshow("Escala de grises", grayScale)
+             cv2.imshow("Region de interes", masked)
+             cv2.imshow("Imagen binaria", threshImg)
 
              if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
