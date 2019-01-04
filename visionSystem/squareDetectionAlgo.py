0f5c53bb9d79a8098d937c3519b1fc069af3b5bb
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-01-03 21:33:24 -0600

    resizing the video

--- visionSystem/squareDetectionAlgo.py
+++ visionSystem/squareDetectionAlgo.py
@@ -1,6 +1,22 @@
 import cv2
 import numpy as np
 
+JUST_ONCE = 0
+def resizingImage(img, f_height, f_width):
+    #cambiando el tamano de la imagen a la mitad
+    ## dividiendo enteros
+    f_height = f_height//2
+    f_width = f_width//2
+
+    global JUST_ONCE
+    #imprimiendo las nuevas medidas
+    if JUST_ONCE == 0:
+        print("Altura: {}, Ancho: {}".format(f_height, f_width))
+        JUST_ONCE = 1
+    
+    img = cv2.resize(img, (f_height, f_width))
+    return img
+
 def mainLoop():
     cap = cv2.VideoCapture("GoPro.mp4")
 
@@ -8,17 +24,18 @@ def mainLoop():
     if cap.isOpened() == False:
         print("Error al procesar el video")
 
-    # cambiando el size para manipular mejor la imagen
+    # capturando tamano de la imagen
     frame_width = int(cap.get(3))
     frame_height = int(cap.get(4))
     print("Altura: {}, Ancho: {}".format(frame_width, frame_height))
-    
+
     # loop principal
     while(cap.isOpened()):
         ret, frame = cap.read()
 
         # procesando la imagen
-        
+        #cambiando tamano de la imagen
+        frame = resizingImage(frame, frame_width, frame_height)
         
         # comprobando que exista un retorno de pantalla
         if ret == True:
