06a7712bab0c89abda9874b188a0746d1b09a1fd
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-02-01 19:47:39 -0600

    formule to get volts to centimeters

--- RobotCodeJava/commandFrankie/src/main/java/frc/robot/commands/ProximitySensorCommand.java
+++ RobotCodeJava/commandFrankie/src/main/java/frc/robot/commands/ProximitySensorCommand.java
@@ -10,9 +10,13 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.smartdashboard.*;
 import edu.wpi.first.wpilibj.AnalogInput;
+//import java.lang.Math;
 
 public class ProximitySensorCommand extends Command {
   AnalogInput sharp_input;
+  double min_raw = 9999;
+  double max_raw = 0;
+  double GUIDE_VALUE = 80 / 3;
 
   public ProximitySensorCommand() {
     // Use requires() here to declare subsystem dependencies
@@ -31,11 +35,20 @@ public class ProximitySensorCommand extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-    int averageVolts = sharp_input.getAverageValue();
-    
-    // esto corre durante todo el modo autonomo
-    // de 5 a 0 V // el sensor mide de 0 a 80cm
-    SmartDashboard.putNumber("SENSOR SHARP", averageVolts);
+    //int averageVolts = sharp_input.getAverageValue();
+
+    double measurement = this.getSensorData();
+    SmartDashboard.putNumber("SENSOR SHARP", measurement);
+    if(measurement > max_raw){
+      // buscando el numero mas grande
+      max_raw = measurement;
+      SmartDashboard.putNumber("MAX", max_raw);
+    }
+    if(measurement < min_raw){
+      // buscando el numero mas pequeno
+      min_raw = measurement;
+      SmartDashboard.putNumber("MIN", min_raw);
+    }
   }
 
   // Make this return true when this Command no longer needs to run execute()
@@ -57,5 +70,22 @@ public class ProximitySensorCommand extends Command {
   @Override
   protected void interrupted() {
     // en caso de ser interrumpido
+  }
+
+  protected double getSensorData() {
+    double raw = sharp_input.getAverageVoltage();
+    // al natural sin calibrar
+    //int raw = sharp_input.getValue();
+    // esto corre durante todo el modo autonomo
+    // de 5 a 0 V // el sensor mide de 0 a 80cm
+    //SmartDashboard.putNumber("SENSOR SHARP", averageVolts);
+    // raw mide entre 0 -4096
+    //int measurement = (int)(28940.1 * Math.pow(raw, -1.16));
+    //int measurement = 4800 / (raw - 20);
+
+    // 1 / volts = distancia -> 0 - 3 v
+
+    double inverse_volts = 1 / raw;
+    return inverse_volts * GUIDE_VALUE;
   }
 }
